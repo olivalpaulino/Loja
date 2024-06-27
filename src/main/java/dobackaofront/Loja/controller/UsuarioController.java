fbfa29c90c80f887633c37af22a3c78b969643ca
unknown <olivalpaulinocc@gmail.com>
2024-06-27 17:53:54 -0300

    atualizando biblioteca java.util

--- src/main/java/dobackaofront/Loja/controller/UsuarioController.java
+++ src/main/java/dobackaofront/Loja/controller/UsuarioController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/usuarios") // raiz da api
 public class UsuarioController {
