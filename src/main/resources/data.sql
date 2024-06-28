1edd74f28007dede5bd632933ae2f419c544e574
unknown <olivalpaulinocc@gmail.com>
2024-06-27 22:57:46 -0300

    não consegui fazer o código ler o arquivo data.sql para inicializar os dados apos o hibernate pegar

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into usuarios(username, password) values("olival", "1123");
+insert into usuarios(username, password) values("jinx", "1123");
+insert into usuarios(username, password) values("mary", "1123");
+insert into usuarios(username, password) values("lary", "1123");
+insert into usuarios(username, password) values("berry", "1123");
+insert into usuarios(username, password) values("garry", "1123");
